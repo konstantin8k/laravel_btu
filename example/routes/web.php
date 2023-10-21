ec164be92b085a2ba0fb9d43ced4e1f1558151cc
student <student@BTU.local>
2023-10-21 16:03:52 +0400

    Initial commit

--- example/routes/web.php
+++ example/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// GET Routes
+Route::get('/about-me', function () {
+    $info = [
+        'first_name' => 'Konstantin',
+        'last_name' => 'khomeriki',
+        'age' => 30,
+        'hobbies' => ['Reading', 'Hiking', 'Coding'],
+    ];
+    return response()->json($info);
+});
+
+Route::get('/other-info-1', function () {
+    $info = [
+        'field1' => 'Value1',
+        'field2' => 'Value2',
+    ];
+    return response()->json($info);
+});
+
+Route::get('/other-info-2', function () {
+    $info = [
+        'field3' => 'Value3',
+        'field4' => 'Value4',
+    ];
+    return response()->json($info);
+});
+
+// POST Route
+Route::post('/update-info', function () {
+    $message = 'წარმატებით განახლდა';
+    $response = ['message' => $message];
+    return response()->json($response);
+});
+
+// PUT Route
+Route::put('/add-info', function () {
+    $message = 'წარმატებით დაემატა';
+    $response = ['message' => $message];
+    return response()->json($response);
+});
+
+// DELETE Route
+Route::delete('/delete-info', function () {
+    $message = 'წარმატებით წაიშალა';
+    $response = ['message' => $message];
+    return response()->json($response);
 });
